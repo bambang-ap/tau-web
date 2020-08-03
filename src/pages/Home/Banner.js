140cdced6666bbcc8906ee195a36e165b236d549
Bambang Adhitya <bambang@kiosawan.com>
2020-08-03 23:32:53 +0700

    h

--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
 const Banner = ({ className = "", ...props }) => {
-	return <button {...props} id="banner" className={`flex flex-col ${className}`}>
-		<button className="flex p-3 pl-10 pr-10 bc-grey-hard c-light f-20">
-			<button className="f-4 flex flex-1 jc-fs">+62 21 5890 8888 atau (+62) 8123 729 0009</button>
-			<button className="f-4 flex flex-1 jc-c">Sistem Informasi Akademik</button>
-			<button className="f-4 flex flex-1 jc-fe ai-c">
+	return <div {...props} id="banner" className={`flex flex-col ${className}`}>
+		<div className="flex p-3 pl-10 pr-10 bc-grey-hard c-light f-20">
+			<div className="f-4 flex flex-1 jc-fs">+62 21 5890 8888 atau (+62) 8123 729 0009</div>
+			<div className="f-4 flex flex-1 jc-c">Sistem Informasi Akademik</div>
+			<div className="f-4 flex flex-1 jc-fe ai-c">
 				<i className="f-6 ml-5 fa fa-instagram" />
 				<i className="f-6 ml-5 fa fa-facebook-square" />
 				<i className="f-6 ml-5 fa fa-twitter-square" />
-			</button>
-		</button>
-		<button className="">
+			</div>
+		</div>
+		<div className="">
 			<img className="" alt="" src={require('src/assets/images/banner.png')} />
-		</button>
-	</button>
+		</div>
+	</div>
 }
 
 export default Banner
