2c8975f35b913e18e39f5646c8e2b41f2d4e5698
Bambang Adhitya <bambang@kiosawan.com>
2020-09-11 15:18:04 +0700

    final?

--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-no-target-blank */
 import React, { useRef, useState, useEffect } from 'react';
 import useWindowSize from 'src/utils/windowSize';
-import { getBanner, FILE_PATH } from 'src/utils/api';
+import { getBanner, FILE_PATH, getManage } from 'src/utils/api';
 import ReactElasticCarousel from 'react-elastic-carousel';
 
 const Banner = ({ className = "", ...props }) => {
@@ -12,9 +12,24 @@ const Banner = ({ className = "", ...props }) => {
 	const carouselRef = useRef()
 	const [, maxHeight, isMobile = false] = useWindowSize()
 	const [height, setHeight] = useState(0)
+	const [manage, setManage] = useState({})
 	const [banner, setBanner] = useState([])
 	const getData = async () => {
+		let manage = {}, manageParts = [
+			'kontakPhone1',
+			'kontakPhone2',
+			'kontakSistemInformasiAkademik',
+			'kontakTwitter',
+			'kontakFacebook',
+			'kontakInstagram'
+		]
 		const { data } = await getBanner({ isMobile })
+		for (let i = 0; i < manageParts.length; i++) {
+			const part = manageParts[i]
+			const { data: { content } } = await getManage({ part })
+			manage[part] = content
+		}
+		setManage(manage)
 		setBanner(data)
 	}
 	const effect = () => {
@@ -26,16 +41,16 @@ const Banner = ({ className = "", ...props }) => {
 	useEffect(effect, [header.clientHeight, maxHeight])
 	return <div {...props} id="banner" className={`flex flex-col ${className}`}>
 		<div ref={infoRef} className={`flex flex-wrap p-3 ${!isMobile && 'pl-10 pr-10'} bc-grey-hard c-light f-20`}>
-			<div className={`${isMobile ? 'f-3 w-full jc-c' : 'f-4 w-1/3 jc-fs'} flex`}>+62 21 5890 8888 atau (+62) 8123 729 0009</div>
-			<div className={`${isMobile ? 'f-3 w-full jc-c' : 'f-4 w-1/3 jc-c'} mt-1 mb-1 flex`}>Sistem Informasi Akademik</div>
+			<div className={`${isMobile ? 'f-3 w-full jc-c' : 'f-4 w-1/3 jc-fs'} flex`}>{manage.kontakPhone1} atau {manage.kontakPhone2}</div>
+			<a className={`${isMobile ? 'f-3 w-full jc-c' : 'f-4 w-1/3 jc-c'} mt-1 mb-1 flex`} target="_blank" href={manage.kontakSistemInformasiAkademik}>Sistem Informasi Akademik</a>
 			<div className={`${isMobile ? 'f-3 w-full jc-c' : 'f-4 w-1/3 jc-fe'} flex ai-c`}>
-				<a target="_blank" href="https://twitter.com/tauniversity">
+				<a target="_blank" href={manage.kontakTwitter}>
 					<i className={`${isMobile ? 'f-4' : 'f-6'} ml-5 fa fa-instagram`} />
 				</a>
-				<a target="_blank" href="https://web.facebook.com/tau.ac.id">
+				<a target="_blank" href={manage.kontakFacebook}>
 					<i className={`${isMobile ? 'f-4' : 'f-6'} ml-5 fa fa-facebook-square`} />
 				</a>
-				<a target="_blank" href="https://www.instagram.com/tauniversity">
+				<a target="_blank" href={manage.kontakInstagram}>
 					<i className={`${isMobile ? 'f-4' : 'f-6'} ml-5 fa fa-twitter-square`} />
 				</a>
 			</div>
